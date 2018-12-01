afd9f83898cb0b30978c4757ba85042dde899a0a
Nicolás Marulanda P <nicomaru@outlook.com>
2018-12-01 15:19:55 +0100

    ArrayUtils - Tests - [Nuevo] Método "isAllNull". Comprueba si todos los valores del array son nulos.

--- src/main/SoftN/Commons/Lang/ArrayUtils.php
+++ src/main/SoftN/Commons/Lang/ArrayUtils.php
@@ -15,4 +15,19 @@ class ArrayUtils {
         return count($array) == 0;
     }
     
+    public static function isAllNull(?array $array, bool $recursive = FALSE): bool {
+        if (ArrayUtils::isEmpty($array)) {
+            return TRUE;
+        }
+        
+        foreach ($array as $v) {
+            if ((!is_null($v) && !is_array($v)) || ($recursive && is_array($v) && !self::isAllNull($v, $recursive))) {
+                return FALSE;
+            }
+        }
+        
+        return TRUE;
+    }
+    
+    
 }
--- src/test/SoftN/Commons/Lang/ArrayUtilsTest.php
+++ src/test/SoftN/Commons/Lang/ArrayUtilsTest.php
@@ -15,4 +15,18 @@ class ArrayUtilsTest extends TestCase {
         $this->assertFalse(ArrayUtils::isEmpty([""]));
         $this->assertFalse(ArrayUtils::isEmpty(["a"]));
     }
+    
+    public function testIsAllNull() {
+        $this->assertTrue(ArrayUtils::isAllNull(NULL));
+        $this->assertTrue(ArrayUtils::isAllNull([NULL]));
+        $this->assertTrue(ArrayUtils::isAllNull([NULL, [NULL]], TRUE));
+        $this->asserttrue(ArrayUtils::isAllNull([NULL, [NULL, [NULL, [NULL, [NULL, NULL]]]]], TRUE));
+        $this->assertFalse(ArrayUtils::isAllNull(["", NULL]));
+        $this->assertFalse(ArrayUtils::isAllNull(["a", [NULL]], TRUE));
+        $this->assertFalse(ArrayUtils::isAllNull(["a", ["b", ["c", ["d", ["f", NULL]]]]], TRUE));
+        $this->assertFalse(ArrayUtils::isAllNull(["a", [NULL]]));
+        $this->assertFalse(ArrayUtils::isAllNull(["a", [NULL, ["a"]]]));
+    }
+    
+    
 }
